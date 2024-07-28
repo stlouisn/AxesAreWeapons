90d755b677dd3d2af3d20dd220a8ebf78a35c76c
stlouisn <stlouisn@hotmail.com>
2024-07-28 10:17:41 -0300

    removed bloat

--- common/src/main/java/io/github/fourmisain/axesareweapons/common/config/AxesAreWeaponsConfig.java
+++ common/src/main/java/io/github/fourmisain/axesareweapons/common/config/AxesAreWeaponsConfig.java
@@ -13,43 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class AxesAreWeaponsConfig implements ConfigData {
 
 	@ConfigEntry.Gui.Tooltip
-	public volatile boolean enableLooting = true;
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean enableKnockback = false;
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean enableFireAspect = false;
-
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean enableModded = false;
-
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean enableForEnchantingTable = true;
-
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean enableDamageInEnchantingTable = true;
-
-	@ConfigEntry.Gui.Tooltip
 	public volatile boolean fastCobWebBreaking = false;
-
-	@ConfigEntry.Category("alltoolsareweapons")
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean pickaxesAreWeapons = false;
-
-	@ConfigEntry.Category("alltoolsareweapons")
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean shovelsAreWeapons = false;
-
-	@ConfigEntry.Category("alltoolsareweapons")
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean hoesAreWeapons = false;
-
-	@ConfigEntry.Category("alltoolsareweapons")
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean rangedWeaponsAreWeapons = false;
-
-	@ConfigEntry.Category("alltoolsareweapons")
-	@ConfigEntry.Gui.Tooltip
-	public volatile boolean enableLootingForRangedWeapons = false;
 
 	@ConfigEntry.Category("alltoolsareweapons")
 	@ConfigEntry.Gui.Tooltip
